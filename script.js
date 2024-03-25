7bfaaa4d3ee7e4c732a8c3a2c6017aa7d609262f
sithh22 <118666999+sithh22@users.noreply.github.com>
2024-03-25 09:56:00 +0200

    123

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var overlayMenu = document.getElementById("overlay-menu");
+    if (overlayMenu.style.display === "block") {
+      overlayMenu.style.display = "none";
+    } else {
+      overlayMenu.style.display = "block";
+    }
+  }
+  
